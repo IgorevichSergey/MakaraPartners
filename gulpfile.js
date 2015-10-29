be81803ecc00aad610519a624313ac45fbefbf3f
Sergey Khristich <skhristich@scenario-projects.com>
2015-10-29 16:39:09 +0200

    fonts and images

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var connect = require("gulp-connect");
 var concat = require('gulp-concat');
-var cssmin = require('gulp-concat-css');
+var cssconcat = require('gulp-concat-css');
 var runSequence = require('run-sequence');
+var uncss = require('gulp-uncss');
 
 config = {
     SCRIPTS: [
@@ -35,15 +36,15 @@ gulp.task('scripts_min', function () {
 });
 
 gulp.task('bower_css_min', function () {
-    gulp.src(config.CSS)
-        .pipe(cssmin('bower.css'))
+    gulp.src(config.BOWER_CSS)
+        .pipe(cssconcat('bower.css'))
         .pipe(gulp.dest('./production/css'));
 });
 
 
 gulp.task('css_min', function () {
     gulp.src(config.CSS)
-        .pipe(cssmin('app.css'))
+        .pipe(cssconcat('app.css'))
         .pipe(gulp.dest('./production/css'));
 });
 
@@ -65,9 +66,15 @@ gulp.task('bower_scripts_min',function () {
         .pipe(concat('bower-components.min.js'))
         .pipe(gulp.dest('./production/js'))
 });
+gulp.task('copy_other', function () {
+    gulp.src('./fonts/**')
+        .pipe(gulp.dest('./production/fonts'));
+    gulp.src('./images/**')
+        .pipe(gulp.dest('./production/images'));
+});
 
 gulp.task('build', function(){
-    runSequence('scripts_min','bower_css_min', 'css_min', 'bower_scripts_min', 'copy_html',function() {
+    runSequence('scripts_min','bower_css_min', 'css_min', 'bower_scripts_min', 'copy_other', 'copy_html',function() {
     });
 
 });
